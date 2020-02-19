548a0b76b98a34053f80e63da7399f640a7f318f
dev_pjh-at-670977422354 <jjjhhhvvv@gmail.com>
2020-02-19 14:11:08 +0900

    Redux Saga middleware - 추가 기능 살펴보기  - select() api : 사가 함수내부에서 스토어에 접근 조회 가능  - throttle() api: 일정 시간 동안 1번의 액션 디스페치만 수행

--- src/modules/counter-with-saga.js
+++ src/modules/counter-with-saga.js
@@ -1,8 +1,17 @@
 import { createAction, handleActions } from 'redux-actions';
-import { delay, put, takeEvery, takeLatest } from 'redux-saga/effects';
+import {
+	delay,
+	put,
+	select,
+	takeEvery,
+	takeLatest,
+	throttle,
+} from 'redux-saga/effects';
 // put:  이벤트 디스패치 함수 ,
 // takeEvery: 이벤트 디스패치 요청에 대해 모두 처리
 // takeLatest : 이벤트 디스패치 요청에 마지막 1개만 처리 : 중복 요청 방지 효과
+// select : saga 함수에서 store 조회 할 때 사용
+// throttle: 일정 시간(주기)를 줘서 패치를 진행 ..
 
 // 1. Action Type
 const INCREASE = 'counter-saga/INCREASE';
@@ -23,7 +32,11 @@ export const decreaseAsync = createAction(DECREASE_SAGA, () => null);
 // saga action creator
 function* increaseSaga() {
 	yield delay(1000); // 1초 대기.
-	yield put(increase()); // { type: INCREASE } 액션을 디스패치
+	yield put(increase()); // { type: INCREASE } 액션을 디스패치:
+	// 스토어에 접근하는 참조 경로 주의할 것.
+	// counter, sagaCounter thunkCounter 등 상태를 분리해서 만들어 놨으므로.
+	const counter = yield select(store => store.sagaCounter);
+	console.log(`사가 함수 내부에서 스토어 접근하여 로깅 counter : ${counter}`);
 }
 
 function* decreaseSaga() {
@@ -35,7 +48,8 @@ function* decreaseSaga() {
 export function* counterSaga() {
 	// takeEvery 는 들어오는 모든 액션에 대해 모두 디스페치를 한다.
 	// 즉 중복된 요청도 모두 처리 한다.
-	yield takeEvery(INCREASE_SAGA, increaseSaga);
+	// yield takeEvery(INCREASE_SAGA, increaseSaga);
+	yield throttle(3000, INCREASE_SAGA, increaseSaga);
 
 	// takeLatest 는 기존에 진행 중이던 작업이 있다면 취소 처리하고
 	// 가장 마지막으로 실행된 액션만 디스페치 한다.
