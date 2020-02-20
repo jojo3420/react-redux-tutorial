04fd9185c6071939cdae49048656d47539164b99
dev_pjh-at-670977422354 <jjjhhhvvv@gmail.com>
2020-02-20 14:02:13 +0900

    사용하지 않는 라우터 제거

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
-import CounterContainer from './containers/CounterContainer';
-import TodoListContainer from './containers/TodoListContainer';
-import CounterWithThunkContainer from './containers/CounterWithThunkContainer';
-import PostWithThunkContainer from './containers/PostWithThunkContainer';
-import CounterWithSagaContainer from './containers/CounterWithSagaContainer';
-import PostWithSagaContainer from './containers/PostWithSagaContainer';
 
 function App() {
 	return (
 		<Switch>
 			<Route path="/" component={Home} />
-			{/*<Route path="/counter" component={CounterContainer} />*/}
-			{/*<Route path="/todo/list" component={TodoListContainer} />*/}
-			{/*<Route path="/counter/thunk" component={CounterWithThunkContainer} />*/}
-			{/*<Route path="/post/thunk" component={PostWithThunkContainer} />*/}
-			{/*<Route path="/counter/saga" component={CounterWithSagaContainer} />*/}
-			{/*<Route path="/post/saga" component={PostWithSagaContainer} />*/}
 		</Switch>
 	);
 }
