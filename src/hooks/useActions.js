2c8902ea72e763cc3c3011fabe2a0ae6df65ed7a
jojo3420 <jjjhhhvvv@gmail.com>
2020-06-24 13:42:07 +0900

    useActions 주석 추가

--- src/hooks/useActions.js
+++ src/hooks/useActions.js
@@ -2,7 +2,13 @@ import { useMemo } from 'react';
 import { useDispatch } from 'react-redux';
 import { bindActionCreators } from "redux";
 
-
+/**
+ * bind dispatch action hooks
+ *
+ * @param actions: Array
+ * @param deps: Array
+ * @returns {unknown} Array of dispatchFn or single dispatch Fn
+ */
 export default function useActions (actions, deps) {
   const dispatch = useDispatch();
   return useMemo(
